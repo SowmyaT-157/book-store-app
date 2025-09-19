b87ab87d6998b77c66bd4d935f3984e442764702
sowmya <sowmya.t@everest.engineering>
2025-09-19 17:52:41 +0530

    Update the root application for remove the search render

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import BookData from './components/book-list';
-import SearchBar from './components/search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchBar />
         <BookData />
       </header>
     </div>
