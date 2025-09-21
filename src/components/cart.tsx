0d47d1fb373ea9fb6aded6a5273fc481d74e2f04
sowmya <sowmya.t@everest.engineering>
2025-09-21 23:52:38 +0530

    Update the code for add a style to the summary

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -51,6 +51,7 @@ export const Cart: React.FC<CartProps> = ({
         <h3>Order Summary</h3>
         <p>Subtotal	₹: {subtotal.toFixed(2)}</p>
         <p>Shipping	₹: {shipping.toFixed(2)}</p>
+        ---------------------------------------
         <p>Total	₹: {total.toFixed(2)}</p>
       </div>
     </div>
