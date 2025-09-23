a8b0aa9bbce5710abdc0aa1b223e6406624b6024
sowmya <sowmya.t@everest.engineering>
2025-09-23 13:51:22 +0530

    Update the login page functionality for user navigate to booklist

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,22 +1,63 @@
+import { useState } from 'react';
 import './login.css'
+import React from 'react';
+import { useEffect } from 'react';
+import {users} from './staticData';
+import {useNavigate} from 'react-router-dom';
+
+
 export const Login = () => {
-   return(
-    <div className='login-page'>
-        <form>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    console.log('Login Component rendered');
+    }, []);
+
+    const handleLogin = () => {
+        console.log(' Login attempt with:', { email, password });
+        const user = users.find(user => user.email === email && user.password === password);
+        if (user) {
+        console.log('Login successful..',user.name);
+        localStorage.setItem('loggedInUser', JSON.stringify(user));
+        navigate('/book-store');
+        } else {
+        console.log(' Login failed');
+        setError('Invalid email or password');
+        }
+    };
+
+    return(
+    <div className='main'>
+        <h3>Books</h3>
+        <div className="form">
             <h3>Login Here</h3>
             <label>Username:</label>
-            <input type="text" placeholder="Email or phone" id="username"/>
+            <input 
+              type="email" 
+              placeholder="Email"
+              value={email} 
+              autoFocus 
+              onChange={e=>{
+                setEmail(e.target.value);
+            } }/>
             
             <label>Password:</label>
-            <input type="password" placeholder="password" id="password" />
-            <button className='button-position'>Login</button>
-            <div className='new-account'>
-               <p>Create new account</p>
-               <button>Register</button>
-            </div>
-            
-        </form>
+            <input 
+              type="password" 
+              placeholder="password"
+              value={password} 
+              onChange={e=>{
+                setPassword(e.target.value);
+            }} />
+            <button className="register-button"onClick={handleLogin}>Login</button>
+            {error && <p className="error-text">{error}</p>}
+            <p>Don't have an account <a href="/">Sign Up</a></p>
+        </div>
     </div>
-   )
+    )
 }
 export default Login;
+
