f233208facaadd09a8a8dfa44e2ccc1f00b52d11
sowmya <sowmya.t@everest.engineering>
2025-09-23 13:53:00 +0530

    Update the app root file for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import Login from './components/login';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
+import SignUp from './components/register';
 import BookData from './components/book-list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <BookData />
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<SignUp/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/book-store" element={<BookData/>}/>
+        </Routes>
+        {/* <BookData /> */}
+         {/* <Login/> */}
+        {/* <SignUp/> */}
+    </BrowserRouter>
   );
 }
 
