14cee60597420faced0a64c45041a95b1ddc5f95
sowmya <sowmya.t@everest.engineering>
2025-09-24 10:56:09 +0530

    Update the component for remove the cart details

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -3,50 +3,23 @@ import "./search.css";
 import { cardItemType } from "../types/prop-type";
 import { BookCard } from "./book-card";
 import { Cart } from "./cart";
+import { useCart } from "./cartContext";
+
 type searchProps = {
   books: cardItemType[];
 };
-// type CartType = {
-//   id: number;
-//   bookName: string;
-//   price: number;
-//   author: string;
-//   bookImage: string;
-//   quantity: number;
-// };
+
+// type cartProps = {
+//     addToCart:(book:cardItemType)=>void;
+//     removeFromCart:(Id:number)=>void;
+//     cart: cardItemType[];
+//     incrementQuantity : (bookId: number) =>void
+//     decrementQuantity : (bookId: number) => void
+// }
 
 export const SearchBar = ({ books }: searchProps) => {
   const [search, setSearch] = useState("");
-  const [cart, setCart] = useState<cardItemType[]>([]);
-
-  const handleAddToCart = (book: cardItemType) => {
-    setCart((previousCart) => {
-      const existingBook = previousCart.find((books) => books.id === book.id);
-      if (existingBook) {
-        return previousCart.map((books) =>
-          books.id === book.id
-            ? { ...books, quantity: books.quantity + 1 }
-            : books,
-        );
-      }
-      console.log(cart);
-      return [...previousCart, { ...book, quantity: 1 }];
-    });
-  };
-  const removeFromCart = (bookId: number) => {
-    setCart((previousCart) =>
-      previousCart.filter((books) => books.id !== bookId)
-    );
-  };
-  const incrementQuantity = (bookId: number) => {
-        setCart(previousCart => previousCart.map(books => books.id === bookId ? { ...books, quantity: books.quantity + 1 } : books)
-        );
-    };
-    const decrementQuantity = (bookId: number) => {
-        setCart(previousCart =>
-            previousCart.map(books => books.id === bookId ? { ...books, quantity: books.quantity - 1 } : books).filter(books => books.quantity > 0)
-        );
-    };
+    const{cart,handleAddToCart,removeFromCart,incrementQuantity,decrementQuantity} = useCart()
 
   const bookSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
