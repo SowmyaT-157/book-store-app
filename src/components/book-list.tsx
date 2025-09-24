1a48f31a03e1f5582a3c4920444ea7c770155fff
sowmya <sowmya.t@everest.engineering>
2025-09-25 01:11:12 +0530

    Update the booklist for remove spaces

--- src/components/book-list.tsx
+++ src/components/book-list.tsx
@@ -1,10 +1,11 @@
-import  { useState, useEffect } from 'react';
+import  {  useEffect } from 'react';
 import './book-list.css'
-import { cardItemType } from '../types/prop-type';
+
 import SearchBar from './search';
+import { useCart } from './cartContext';
 
 const BookData = () => {
-  const [cards, setCards] = useState<cardItemType[]>([])
+  const {cards,setCards}=useCart()
   const fetchData = async() => {
     let a = await fetch('  https://68ca8400430c4476c349caf6.mockapi.io/books/Books')
     let data = await a.json()
