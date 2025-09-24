ac257b07fab7af78fc741c8f9399ae68678c2280
sowmya <sowmya.t@everest.engineering>
2025-09-24 11:09:21 +0530

    Update the cart into cart context

---
+++ src/components/cartContext.tsx
@@ -0,0 +1,58 @@
+import { useContext,createContext, ReactNode, } from "react"
+import { useState } from "react"
+import { cardItemType } from "../types/prop-type"
+
+export interface CartContextProps {
+  cart: cardItemType[];
+  removeFromCart : (bookId: number) =>void;
+  incrementQuantity : (bookId: number) =>void;
+  decrementQuantity : (bookId: number) => void;
+  handleAddToCart: (book: cardItemType) => void;
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<cardItemType[]>([]);
+  
+const handleAddToCart = (book: cardItemType) => {
+    setCart((previousCart) => {
+      const existingBook = previousCart.find((books) => books.id === book.id);
+      if (existingBook) {
+        return previousCart.map((books) =>
+          books.id === book.id
+            ? { ...books, quantity: books.quantity + 1 }
+            : books,
+        );
+      }
+      console.log(cart);
+      return [...previousCart, { ...book, quantity: 1 }];
+    });
+  };
+   const removeFromCart = (bookId: number) => {
+    setCart((previousCart) =>
+      previousCart.filter((books) => books.id !== bookId)
+    );
+  };
+  const incrementQuantity = (bookId: number) => {
+        setCart(previousCart => previousCart.map(books => books.id === bookId ? { ...books, quantity: books.quantity + 1 } : books)
+        );
+    };
+    const decrementQuantity = (bookId: number) => {
+        setCart(previousCart =>
+            previousCart.map(books => books.id === bookId ? { ...books, quantity: books.quantity - 1 } : books).filter(books => books.quantity > 0)
+        );
+    };
+  return (
+      <CartContext.Provider value={{ cart,handleAddToCart,removeFromCart, incrementQuantity,decrementQuantity  }}>
+        {children}
+      </CartContext.Provider>
+    );
+  };
+  export const useCart = (): CartContextProps => {
+    const context = useContext(CartContext);
+    if (!context) {
+      throw new Error('useWishlist must be used within a Wishlist Provider');
+    }
+    return context;
+  };
