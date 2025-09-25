e7ddf700b0215c3758602676c8f96a1f9e4b49be
sowmya <sowmya.t@everest.engineering>
2025-09-25 14:38:24 +0530

    Update the root of the application for routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import BookDetailPage from './components/book-details';
 import { WishlistProvider } from './components/wishlist-data';
 import { CartProvider } from './components/cartContext';
 import AuthProvider from './components/userAuthentication';
+import Wishlist from './components/wishlist';
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
                 <Route path="/" element={<SignUp/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/book-store" element={<BookData/>}/>
-                <Route path="/book-details" element={<BookDetailPage books={[]}/>}/>
+                <Route path="/book-details" element={<BookDetailPage/>}/>
+                <Route path="/wishlist"element={<Wishlist/>}/>
             </Routes>
           </AuthProvider>
         </CartProvider>
