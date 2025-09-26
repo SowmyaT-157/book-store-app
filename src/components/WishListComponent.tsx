64fe11ebc0ee3d33855ec32a17bbf8c7ccbd2a2d
sowmya <sowmya.t@everest.engineering>
2025-09-26 13:57:59 +0530

    Update the wish list component for change the file names

---
+++ src/components/WishListComponent.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useWishlist } from "../context/WishlistData";
+import { useCart } from "../context/CartContextToHandleCaer";
+import { useAuth } from "../context/Authentication";
+import "../styles/wishlist.css"
+
+const Wishlist: React.FC = () => {
+  const { wishlist } = useWishlist();
+  const { cart, handleAddToCart, removeFromCart } = useCart();
+    const { users } = useAuth();
+    console.log(users)
+  
+  return (
+    <div>
+       <div className="wishlist-page">
+        <div className="wishlist-header">
+          <h1>Wishlist</h1>
+          {users && (
+            <div className="user-block">
+                   User:{users.name} 
+              <div>Email:{users.email}</div>
+            </div>
+          )}
+        </div>
+        <div className="wishlist-grid">
+          {wishlist.length === 0 ? (
+            <p>No books in wishlist</p>
+          ) : (
+            wishlist.map((book) => {
+              const inCart = cart.some((c) => c.id === book.id);
+              return (
+                <div key={book.id} className="wishlist-card">
+                  <div className="list-inside"><img src={book.bookImage} alt={book.bookName} /></div>
+                  <div>
+                  <h4>{book.bookName}</h4>
+                  <p>{book.author}</p>
+                  <p>₹{book.price}</p>
+                  <div>
+                    <button
+                      onClick={() =>
+                        inCart ? removeFromCart(book.id) : handleAddToCart(book)
+                      }
+                      className={inCart ? "remove" : "cart"}
+                    >
+                      {inCart ? "Remove from Cart" : "Add to Cart"}
+                    </button>
+                  </div>
+                </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      </div>
+  )}
+export default Wishlist;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
